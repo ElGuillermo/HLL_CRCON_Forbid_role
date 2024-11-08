3bc2ab2d4f2d20a3808b178cacea94bf9aa16e07
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-11-08 18:51:45 +0100

    Update automod_forbid_role.py

--- hll_rcon_tool/custom_tools/automod_forbid_role.py
+++ hll_rcon_tool/custom_tools/automod_forbid_role.py
@@ -1,5 +1,5 @@
 """
-automod_forbid_role.py
+automod_forbid_role
 
 A plugin for HLL CRCON (https://github.com/MarechJ/hll_rcon_tool)
 that blocks role(s) access to defined players.
@@ -16,7 +16,8 @@ import discord
 from rcon.rcon import Rcon
 from rcon.settings import SERVER_INFO
 from rcon.utils import get_server_number
-from custom_tools.custom_common import (
+from custom_tools.automod_forbid_role_config import *
+from custom_tools.common_functions import (
     CLAN_URL,
     DISCORD_EMBED_AUTHOR_URL,
     DISCORD_EMBED_AUTHOR_ICON_URL,
@@ -24,104 +25,7 @@ from custom_tools.custom_common import (
     get_avatar_url,
     get_external_profile_url
 )
-from custom_tools.custom_translations import TRANSL
-
-
-# Configuration (you must review/change these !)
-# -----------------------------------------------------------------------------
-
-# Don't forget you have some parameters to set in 'custom_common.py' too !
-
-# True : only logs - no real message sent / no real punition applied)
-# Value : True or False
-TEST_MODE = False
-
-# The script can work without any Discord output
-# False : the only output would be the log file (as set in config/supervisord.conf)
-USE_DISCORD = True
-
-# Dedicated Discord's channel webhook
-# ServerNumber, Webhook, Enabled
-SERVER_CONFIG = [
-    ["https://discord.com/api/webhooks/...", True],  # Server 1
-    ["https://discord.com/api/webhooks/...", False],  # Server 2
-    ["https://discord.com/api/webhooks/...", False],  # Server 3
-    ["https://discord.com/api/webhooks/...", False],  # Server 4
-    ["https://discord.com/api/webhooks/...", False],  # Server 5
-    ["https://discord.com/api/webhooks/...", False],  # Server 6
-    ["https://discord.com/api/webhooks/...", False],  # Server 7
-    ["https://discord.com/api/webhooks/...", False],  # Server 8
-    ["https://discord.com/api/webhooks/...", False],  # Server 9
-    ["https://discord.com/api/webhooks/...", False]  # Server 10
-]
-
-# Discord embeds strings translations
-# Available : 0 for english, 1 for french, 2 for german
-LANG = 0
-
-# The interval between watch turns (in seconds)
-# Recommended : as the stats must be gathered for all the players,
-#               requiring some amount of data from the game server,
-#               you may encounter slowdowns if done too frequently.
-# Default : 60
-WATCH_INTERVAL_SECS = 40
-
-# Define the guys and the roles they can't play
-# action : Choose either "warning" (or "message"), "punish" or "kick"
-# note : "punish" will report failures as long as the player isn't alive on map
-OBSERVED_PLAYERS = [
-    # {
-    #     "id": "76561234562504900",  # Someguy
-    #     "roles": ["armycommander", "officer"],
-    #     "reason": "No voice comms",
-    #     "action": "punish"
-    # }
-    # {
-    #     "id": "76561123456004778",  # SomeOtherGuy
-    #     "roles": ["armycommander"],
-    #     "reason": "Toxic",
-    #     "action": "punish"
-    # }
-]
-
-
-# Miscellaneous (you don't have to change these)
-# ----------------------------------------------
-
-# {} replace values in this order :
-# pseudo (str), role (str), reason (str), clan url (automatic)
-# ie : "Sorry, {} !\n\nYou can't play\n'{}'\non this server.\n\nReason: {}\n\nContact: {}"
-WARNING_MSG = (
-    "(Message automatique)\n\n"
-    "Attention, {} !\n\n"
-    "Tu joues\n\n"
-    "{}\n\n"
-    "alors que cela t'est interdit sur ce serveur.\n\n"
-    "Raison :\n"
-    "{}\n\n"
-    "Réclamations :\n{}"
-)
-
-# {} replace values in this order :
-# pseudo (str), role (str)
-# ie : "Sorry, {} !\n\nYou can't play\n'{}'\non this server.\n\nReason: {}\n\nContact: {}"
-PUNISH_MSG = (
-    "\n(Message automatique)\n\n"
-    "Désolé, {} !\n\n"
-    "Tu as été puni parce que tu joues\n\n"
-    "{}\n\n"
-    "alors que cela t'est interdit sur ce serveur.\n\n"
-    "Raison :\n"
-    "{}\n\n"
-    "Réclamations :\n{}\n\n"
-)
-
-# Bot name that will be displayed in CRCON "audit logs" and Discord embeds
-BOT_NAME = "CRCON_forbid_role"
-
-
-# (End of configuration)
-# -----------------------------------------------------------------------------
+from custom_tools.common_translations import TRANSL
 
 
 def filter_players():
